a030c879a54cf67d1b1b78d2a1d38e5be6418cde
Tung Lam Pham <tung.lam-pham@source.kitchen>
2024-07-16 15:32:55 +0200

    feat: Implement custom `MultiChildRenderObjectWidget` widget

--- lib/src/challenge.dart
+++ lib/src/challenge.dart
@@ -1,20 +1,71 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
-final class LiveCodingTest extends StatelessWidget {
-  final List<Widget> children;
+class LiveCodingTestParentData extends ContainerBoxParentData<RenderBox> {}
+
+class LiveCodingTest extends MultiChildRenderObjectWidget {
+  final Alignment alignment;
 
   const LiveCodingTest({
     super.key,
-    required this.children,
+    required super.children,
+    this.alignment = Alignment.center,
   });
 
   @override
-  Widget build(BuildContext context) {
-    return SizedBox.expand(
-      child: Stack(
-        alignment: Alignment.center,
-        children: children,
-      ),
-    );
+  RenderLiveCodingTest createRenderObject(BuildContext context) {
+    return RenderLiveCodingTest(alignment);
+  }
+
+  @override
+  void updateRenderObject(BuildContext context, RenderLiveCodingTest renderObject) {
+    renderObject.alignment = alignment;
+  }
+}
+
+class RenderLiveCodingTest extends RenderBox
+    with ContainerRenderObjectMixin<RenderBox, LiveCodingTestParentData>, RenderBoxContainerDefaultsMixin<RenderBox, LiveCodingTestParentData> {
+  Alignment alignment;
+
+  RenderLiveCodingTest(this.alignment);
+
+  /// Must be override to use [LiveCodingTestParentData]
+  @override
+  void setupParentData(RenderBox child) {
+    if (child.parentData is! LiveCodingTestParentData) {
+      child.parentData = LiveCodingTestParentData();
+    }
+  }
+
+  @override
+  void performLayout() {
+    // Layout children widgets to get their `Size` values.
+    RenderBox? child = firstChild;
+    while (child != null) {
+      final childParentData = child.parentData as LiveCodingTestParentData?;
+      child.layout(constraints.loosen(), parentUsesSize: true);
+      child = childParentData?.nextSibling;
+    }
+
+    // Position each child within the `RenderLiveCodingTest` size based on it's `constraints`
+    size = Size(constraints.maxWidth, constraints.maxHeight);
+    child = firstChild;
+    while (child != null) {
+      final childParentData = child.parentData as LiveCodingTestParentData?;
+      final dx = (size.width - child.size.width) / 2 * (alignment.x + 1);
+      final dy = (size.height - child.size.height) / 2 * (alignment.y + 1);
+      childParentData?.offset = Offset(dx, dy);
+      child = childParentData?.nextSibling;
+    }
+  }
+
+  @override
+  void paint(PaintingContext context, Offset offset) {
+    RenderBox? child = firstChild;
+    while (child != null) {
+      final childParentData = child.parentData as LiveCodingTestParentData?;
+      context.paintChild(child, offset + (childParentData?.offset ?? Offset.zero));
+      child = childParentData?.nextSibling;
+    }
   }
 }
