2522bb90aa5145a51c0ffa533929bec1f3f4084b
Tung Lam Pham <tung.lam-pham@source.kitchen>
2024-07-17 09:12:09 +0200

    feat: Implement `generateRandomLiveCodingContainers` for `LiveCodingContainer` generation

--- lib/src/app/home.dart
+++ lib/src/app/home.dart
@@ -1,32 +1,55 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:live_coding/src/app/container.dart';
 import 'package:live_coding/src/challenge.dart';
 
 final class LiveCodingHome extends StatelessWidget {
-  const LiveCodingHome({super.key});
-
-  static const _children = [
-    LiveCodingContainer(
-      dimension: 300,
-      color: Colors.red,
-    ),
-    LiveCodingContainer(
-      dimension: 200,
-      color: Colors.blue,
-    ),
-    LiveCodingContainer(
-      dimension: 100,
-      color: Colors.purple,
-    ),
+  static const _colors = [
+    Colors.red,
+    Colors.blue,
+    Colors.purple,
+    Colors.green,
+    Colors.orange,
+    Colors.yellow,
+    Colors.pink,
+    Colors.cyan,
+    Colors.brown,
+    Colors.grey,
+    Colors.teal,
+    Colors.indigo,
+    Colors.lime,
+    Colors.amber,
+    Colors.deepOrange,
+    Colors.deepPurple,
+    Colors.lightBlue,
+    Colors.lightGreen,
+    Colors.blueGrey,
+    Colors.black,
   ];
+
+  const LiveCodingHome({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final children = _children.toList();
     return Scaffold(
       body: LiveCodingTest(
-        children: children..shuffle(),
+        children: generateRandomLiveCodingContainers(5000).toList()..shuffle(),
       ),
     );
+  }
+
+  Iterable<LiveCodingContainer> generateRandomLiveCodingContainers(int n) sync* {
+    final random = Random();
+
+    for (int i = 0; i < n; i++) {
+      final dimension = 50 + random.nextInt(800);
+      final color = _colors[random.nextInt(_colors.length)];
+
+      yield LiveCodingContainer(
+        dimension: dimension.toDouble(),
+        color: color,
+      );
+    }
   }
 }
